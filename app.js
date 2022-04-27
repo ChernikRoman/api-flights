08aee0375d539b274c2e5300c13ee1c7c506eac0
Roman Chernik <chernik.r@yandex.ru>
2022-04-27 15:51:19 +0300

    feat: add route '/flights'

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.get('/flights', (req, res) => {
+    try {
+        const flights = require(__dirname + '/static/flights.json');
+        res.status(200).send(flights);
+    } catch {
+        res.status(500).send('Error');
+    }
+})
+
+app.use((req, res) => {
+    res.send('Path not found')
+})
+
 app.listen(3000, () => {
     console.log('Server has been started...');
 })
