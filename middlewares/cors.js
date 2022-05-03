dfbe7e36d2dfe97655434793fc4b230121015d59
Roman Chernik <chernik.r@yandex.ru>
2022-05-03 21:26:46 +0300

    fix: change cors rules

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,18 +1,12 @@
 function corsHandler(req, res, next) {
   const allowedOrigin = [
-    'https://flights-three.vercel.app',
-    'http://flights-three.vercel.app',
-    'http://localhost:3000',
-    'http://localhost:3001',
-  ];
+
   const allowedMethods = 'GET,PUT,HEAD,PATCH,DELETE,POST';
   const allowedHeaders = req.headers['access-control-request-headers'];
   const { origin } = req.headers;
 
-  if (allowedOrigin.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', allowedMethods);
